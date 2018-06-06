798894b4ffcd6f43d534ef4857f7f1172619e2d9
B V Suresh <gandipalem@gmail.com>
2018-06-07 00:34:59 +0530

    Update combineFeatures.R

--- R/combineFeatures.R
+++ R/combineFeatures.R
@@ -1,8 +1,1 @@
-combineFeatures <- function(seq,csv) {
-source("seqCTD.R")
-ctd <- seqCTD(seq)
-seqCSV <- read.csv("csv",header=FALSE)
-seqCSV <- t(seqCSV[,-1])
-mat <- rbind(seqCSV,ctd)
-return(mat)
-}
+
